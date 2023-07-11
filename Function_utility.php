a9dfda345006272640ad782b57259df42d9b8242
Alessandro Ferranti <124639783+Alessandro-Ferranti@users.noreply.github.com>
2023-07-11 17:07:51 +0200

    Delete Function_utility.php

--- Function_utility.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-function check_login(){
-    // Verifica se l'utente ha effettuato l'accesso
-    if (isset($_SESSION['Username_utente']) && $_SESSION['Username_utente'] != "") {
-        return TRUE;
-    }
-  }
- ?>
